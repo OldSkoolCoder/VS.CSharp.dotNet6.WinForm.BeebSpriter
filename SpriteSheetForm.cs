eb2df62ece3d919a6974359a3c30ffe1fbd6b56e
derek <derek@sorcie.co.uk>
2023-10-02 21:21:04 +0100

    changes to exit. added check for unsaved and ask to save before leaving

--- SpriteSheetForm.cs
+++ SpriteSheetForm.cs
@@ -476,6 +476,30 @@ namespace BeebSpriter
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            if (IsUnsaved)
+            {
+                DialogResult result = MessageBox.Show("You have unsaved work. Do you wish to save before exiting?",
+                                                      "Warning",
+                                                      MessageBoxButtons.YesNoCancel);
+
+                if (result == DialogResult.Yes)
+                {
+                    result = saveFileDialog1.ShowDialog();
+                    if (result == DialogResult.OK)
+                    {
+                        Save();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
             System.Windows.Forms.Application.Exit();
         }
         /// <summary>
