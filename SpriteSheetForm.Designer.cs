19a4b6656fd555b3853c56b9e5d43da29d5eaa14
shaunlindsley <shaun.lindsley@ntlworld.com>
2023-10-31 14:14:05 +0000

    Added global Colour Replacer to the sprite sheet

--- SpriteSheetForm.Designer.cs
+++ SpriteSheetForm.Designer.cs
@@ -67,6 +67,7 @@
             toolStripMenuItem5 = new System.Windows.Forms.ToolStripSeparator();
             ImportImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ImportSpritePadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ImportFromSEUCKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
@@ -97,7 +98,9 @@
             toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             ZoomOutoolStripButton = new System.Windows.Forms.ToolStripButton();
             ZoomInToolStripButton = new System.Windows.Forms.ToolStripButton();
-            ImportFromSEUCKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            colourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ReplaceColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             menuStrip1.SuspendLayout();
             contextMenu.SuspendLayout();
             spriteContextMenu.SuspendLayout();
@@ -107,7 +110,7 @@
             // 
             // menuStrip1
             // 
-            menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { fileToolStripMenuItem, viewToolStripMenuItem, toolsToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, viewToolStripMenuItem, toolsToolStripMenuItem, helpToolStripMenuItem });
             menuStrip1.Location = new System.Drawing.Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Size = new System.Drawing.Size(405, 24);
@@ -360,6 +363,13 @@
             ImportSpritePadToolStripMenuItem.Text = "Import from SpritePad";
             ImportSpritePadToolStripMenuItem.Click += ImportSpritePadToolStripMenuItem_Click;
             // 
+            // ImportFromSEUCKToolStripMenuItem
+            // 
+            ImportFromSEUCKToolStripMenuItem.Name = "ImportFromSEUCKToolStripMenuItem";
+            ImportFromSEUCKToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            ImportFromSEUCKToolStripMenuItem.Text = "Import from SEUCK";
+            ImportFromSEUCKToolStripMenuItem.Click += ImportFromSEUCKToolStripMenuItem_Click;
+            // 
             // helpToolStripMenuItem
             // 
             helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { aboutToolStripMenuItem });
@@ -598,12 +608,27 @@
             ZoomInToolStripButton.Text = "Zoom In";
             ZoomInToolStripButton.Click += ZoomInToolStripButton_Click;
             // 
-            // ImportFromSEUCKToolStripMenuItem
+            // editToolStripMenuItem
             // 
-            ImportFromSEUCKToolStripMenuItem.Name = "ImportFromSEUCKToolStripMenuItem";
-            ImportFromSEUCKToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
-            ImportFromSEUCKToolStripMenuItem.Text = "Import from SEUCK";
-            ImportFromSEUCKToolStripMenuItem.Click += ImportFromSEUCKToolStripMenuItem_Click;
+            editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { colourToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // colourToolStripMenuItem
+            // 
+            colourToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { ReplaceColourToolStripMenuItem });
+            colourToolStripMenuItem.Name = "colourToolStripMenuItem";
+            colourToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            colourToolStripMenuItem.Text = "Colour";
+            // 
+            // ReplaceColourToolStripMenuItem
+            // 
+            ReplaceColourToolStripMenuItem.Image = Properties.Resources.Colours;
+            ReplaceColourToolStripMenuItem.Name = "ReplaceColourToolStripMenuItem";
+            ReplaceColourToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            ReplaceColourToolStripMenuItem.Text = "Replace Colour...";
+            ReplaceColourToolStripMenuItem.Click += ReplaceColourToolStripMenuItem_Click;
             // 
             // SpriteSheetForm
             // 
@@ -704,6 +729,9 @@
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem5;
         private System.Windows.Forms.ToolStripMenuItem ImportSpritePadToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ImportFromSEUCKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ReplaceColourToolStripMenuItem;
     }
 }
 
